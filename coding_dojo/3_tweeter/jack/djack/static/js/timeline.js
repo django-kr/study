d3dfe4b8999e33f81a01ef7676d9e63c6ee35965
Jeong-Min Lee <falsetru@gmail.com>
2014-03-29 15:46:52 +0900

    Render all posts (tweets).

--- coding_dojo/3_tweeter/jack/djack/static/js/timeline.js
+++ coding_dojo/3_tweeter/jack/djack/static/js/timeline.js
@@ -1,13 +1,26 @@
 /** @jsx React.DOM */
 
 $(function(){
+    var Post = React.createClass({
+        render: function() {
+            return (
+            <div className="post">
+                <h3>Tweet {this.props.data.id}</h3>
+                <p>{this.props.data.text} posted by {this.props.data.writer_id}</p>
+            </div>
+            );
+        }
+    });
+
     $.ajax({
-        url: '/jack/timeline/1',
+        url: '/jack/timeline/1', // XXX 1
         dataType: 'json',
         success: function(data) {
-            console.log(data);
+            var posts = data.map(function(post) {
+                return <Post data={post}></Post>
+            });
             React.renderComponent(
-                <p>{data[0].text}</p>,
+                <p>{posts}</p>,
                 document.getElementById('example')
             );
         }
